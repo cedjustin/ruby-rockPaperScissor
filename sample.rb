e06bd6630dcc221fa2a9671248d943716f154b93
root <root@DESKTOP-3IBCSAR.localdomain>
2020-03-23 17:23:12 +0200

    reshaped the codes

--- sample.rb
+++ sample.rb
@@ -1,49 +1,55 @@
-class Player
+class Player  
     def hand
         puts "Enter a number"
-        i = gets.to_i
-        if(i > 2 || i < 0)
-            p "Please enter a number from 0 to 2"
-        else
-            return i
+        puts "0 : Goo"
+        puts "1 : Choki"
+        puts "2 : Par"      
+
+        player_input = gets.chomp            
+
+        while player_input.to_i > 2 || player_input.to_i < 0 do
+            puts "Enter a number between 0-2" 
+            player_input = gets.chomp
         end
+        
+        return player_input   
     end
 end
-
+    
 class Enemy
     def hand
-        e = rand(0..2)
-        e
+        enemy_input = rand(0..2)
     end
 end
-
-class Game
+    
+class Janken
     def pon(player_hand, enemy_hand)
-        if(player_hand != "Please enter a number from 0 to 2")
-            p "players hand: " + player_hand.to_s
-            p "enemy hand: " + enemy_hand.to_s
-            result = ((player_hand - enemy_hand) + 3) % 3
-            if (result == 0)
-                p "its draw, please continue to play"
-            elsif (result == 1)
-                p "You Lose"
-            elsif (result == 2)
-                p "You Win"
-            else
-                p "Not Possible"
-            end
+        if enemy_hand.to_i == 0
+            rival_hand = "Goo"
+        elsif enemy_hand.to_i == 1
+            rival_hand = "Choki"
         else
-            p "Restart the game"
+            rival_hand = "Par"
         end
-        # Create a method in it that will cause the player to hit the value you typed in and the one that Enemy randomly gave out, and output the result on the console
-        # At that time, when you enter values other than draw or rock, scissors, paper, do another game.
-        # Show what the other party gave out of rock, scissors, paper
+        
+        result = ((player_hand.to_i - enemy_hand.to_i) + 3) % 3    
+
+        if result == 0
+            puts "The hand of the other party is #{rival_hand}.It is a draw Please try again"
+            player = Player.new
+            enemy = Enemy.new
+            janken = Janken.new
+            janken.pon(player.hand, enemy.hand)
+        elsif result == 1
+            puts "The hand of the other party is #{rival_hand}.You loose" 
+        elsif result == 2
+            puts "The hand of the other party is #{rival_hand}.you are the winner"      
+        end   
     end
 end
-   
+    
 player = Player.new
 enemy = Enemy.new
-game = Game.new
-   
-   # The Game method is invoked by the following description
-game.pon(player.hand, enemy.hand)
+janken = Janken.new
+     
+janken.pon(player.hand, enemy.hand)
